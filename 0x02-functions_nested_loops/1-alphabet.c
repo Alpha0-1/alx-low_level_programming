ef067b13df2bdfc1f10a04c55c47a53fa15f5a5b
Alpha0-1 <alphaomolo142@gmail.com>
2023-02-21 01:26:06 -0800

    print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function which prints all alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+
+	char g;
+
+	for (g = 'a'; g <= 'z'; g++)
+	{
+		_putchar(g);
+	}
+	_putchar('\n') }
