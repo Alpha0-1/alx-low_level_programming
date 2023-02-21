b5dbfcc80d145d11cd0db47dcac27b416e796243
Alpha0-1 <alphaomolo142@gmail.com>
2023-02-20 22:23:48 -0800

    Header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ /**
+ * main - Entry point of the program
+ *
+ * This program prints the character '_' using the _putchar function,
+ * followed by a new line character using the _putchar function again.
+ * There are no input parameters. The program returns 0 on success.
+ *
+ */
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
+
+	return (0); }
+
