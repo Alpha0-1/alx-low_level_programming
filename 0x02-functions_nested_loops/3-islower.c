d3744bc4f628e63522b033ce41ed1f0575a0d5d5
Alpha0-1 <alphaomolo142@gmail.com>
2023-02-21 03:45:19 -0800

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ *
+ * @c : the character(c)
+ *
+ * Return : 1 if lowercase, 0 if uppercase
+ *
+ */
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0); }
