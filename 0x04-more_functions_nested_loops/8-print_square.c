4ff38dfb29295645576009fd2b67a6e44e95d1b4
Alpha0-1 <alphaomolo142@gmail.com>
2023-02-23 02:08:33 -0800

    Print squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,29 +3,32 @@
 /**
  * print_square - prints a square using the '#' character
  * @size: size of the square to be printed
- * Description: This function prints a square using the '#' character
- * with the size specified by the parameter 'size'. If the size is 0 or
+ *
+ * Description: This function prints a square
+ * using the '#' character
+ * with the size specified by the parameter 'size'.
+ * If the size is 0 or
  * less, the function only prints a newline character.
  */
 void print_square(int size)
 {
-
-	int co, ro;
-
 	if (size <= 0)
 	{
 	putchar('\n');
 	}
 	else
 	{
-	for (co = 1; co <= size; co++)
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+	for (j = 1; j <= size; j++)
 	{
 	putchar('#');
+	}
 
-	for (ro = 2; ro <= size; ro++)
-	{
 	putchar('\n');
 	}
 	}
-	}
 }
+
