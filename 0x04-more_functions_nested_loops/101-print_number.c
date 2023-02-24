2866553efd37b429a4a9d499a3e0a2997b7c3bae
Alpha0-1 <alphaomolo142@gmail.com>
2023-02-23 16:07:08 -0800

    print individual numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,33 +3,38 @@
 #include <stdlib.h>
 
 /**
- * print_number - prints an integer using _putchar
- * @n: the integer to be printed
+ * print_number - Prints an integer using _putchar function
+ *
+ * @n: The integer to be printed
  *
  * Return: void
  */
 void print_number(int n)
 {
-	unsigned int num, div = 1;
-
 	if (n < 0)
 	{
 	putchar('-');
-	num = -n;
+	n = -n;
 	}
-	else
+	if (n / 10 != 0)
 	{
-	num = n;
+	print_number(n / 10);
 	}
+	putchar((n % 10) + '0');
+}
 
-	while (num / div > 9)
-	div *= 10;
-
-	while (div != 0)
-	{
-	putchar(num / div + '0');
-	num %= div;
-	div /= 10;
-	}
+int main(void)
+{
+	print_number(98);
+	putchar('\n');
+	print_number(402);
+	putchar('\n');
+	print_number(1024);
+	putchar('\n');
+	print_number(0);
+	putchar('\n');
+	print_number(-98);
+	putchar('\n');
+	return (0);
 }
 
