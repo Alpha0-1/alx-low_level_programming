190b22c69d8544f9aca002e5b5d7ae61a79e5a31
Alpha0-1 <alphaomolo142@gmail.com>
2023-03-05 21:43:40 -0800

    2-strchr

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,11 +7,11 @@
  *
  * Return: a pointer to the memory area dest.
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	char *destination = dest;
+	const char *source = src;
 
 	for (index = 0; index < n; index++)
 		destination[index] = source[index];
