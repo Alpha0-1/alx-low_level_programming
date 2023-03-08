ab51dc32d76a38d743175d2f98958b854e83d459
Alpha0-1 <alphaomolo142@gmail.com>
2023-03-08 04:05:53 -0800

    100-palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,27 +1,32 @@
 #include "main.h"
 /**
  * is_palindrome - Checks if a string is a palindrome
- *
  * @s: Pointer to the first character of the string
  *
  * Return: 1 if the string is a palindrome, 0 otherwise
  */
 int is_palindrome(char *s)
 {
-	int len, i;
+	int len = 0;
+	char *p1, *p2;
 
 	if (s == NULL)
-	return (0);
+		return (0);
 
-	len = 0;
 	while (s[len] != '\0')
-	len++;
+		len++;
 
-	for (i = 0; i < len / 2; i++)
+	p1 = s;
+	p2 = s + len - 1;
+
+	while (p2 > p1)
 	{
-	if (s[i] != s[len - i - 1])
-	return (0);
+		if (*p1 != *p2)
+			return (0);
+		p1++;
+		p2--;
 	}
 
 	return (1);
 }
+
