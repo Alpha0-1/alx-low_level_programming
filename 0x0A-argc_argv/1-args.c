84e18ec0da23cba65e59c72ffa5e5c012cbe5cd6
Alpha0-1 <alphaomolo142@gmail.com>
2023-03-09 23:41:49 -0800

    Task one and two

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the number of arguments passed into the program
+ * @argc: Number of arguments passed
+ * @argv: An array of strings containing arguments to be passed
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
