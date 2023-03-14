c5fcece069e2f13283be2d27d5fa494a6ec797e4
Alpha0-1 <alphaomolo142@gmail.com>
2023-03-14 14:17:14 -0700

    2-str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,29 +30,30 @@ int _strlen(char *s)
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *dst;
-	unsigned int i, j, size;
+	char *concat_str;
+	int s1_len = 0, s2_len = 0, i, j;
 
 	if (s1 == NULL)
-		s1 = "";
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
 
-	size = (_strlen(s1) + _strlen(s2) + 1);
+	while (s1[s1_len] != '\0')
+	s1_len++;
+	while (s2[s2_len] != '\0')
+	s2_len++;
 
-	dst = (char *) malloc(size * sizeof(char));
+	concat_str = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (concat_str == NULL)
+	return (NULL);
 
-	if (dst == 0)
-	{
-		return (NULL);
-	}
+	for (i = 0; i < s1_len; i++)
+	concat_str[i] = s1[i];
 
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(dst + i) = *(s1 + i);
+	for (j = 0; j < s2_len; j++)
+	concat_str[i + j] = s2[j];
+	concat_str[i + j] = '\0';
 
-	for (j = 0; *(s2 + j) != '\0'; j++)
-	{
-		*(dst + i) = *(s2 + j);
-		i++;
-	}
-	return (dst);
+	return (concat_str);
 }
 
