76adfa6bb1a7856d979a1ee7a6a7f0e4407c7d4d
Alpha0-1 <alphaomolo142@gmail.com>
2023-03-20 10:50:44 -0700

    f

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,5 +23,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *newdog(char *name, float age, char *owner);
 int main(void);
+void free_dog(dog_t *d);
 
 #endif
