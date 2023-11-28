1ab39fa9405138639d5b4650ef8daf65cf39c475
Alpha0-1 <alphaomollo142@gmail.com>
2023-11-28 11:18:27 -0800

    2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - entry point of the program
+ * description: this program prints the name of the file
+ * it was compiled from, followed by a new line.
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
 	printf("The current file is: %s\n", __FILE__);
-	return 0;
+	return (0);
 }
 
